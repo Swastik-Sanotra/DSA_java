a54860fc6670599a2132d6776b0f024efc240b28
Swastik Sanotra <swastiksanotra05@gmail.com>
2026-05-06 10:14:15 +0530

    added first java program

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,8 @@
 public class Arrays {
-    
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 2};
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
 }
